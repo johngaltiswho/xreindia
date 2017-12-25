13b29c2dd9477af10f7437959960ba8c6e62d6a4
johngaltiswho <umamaheshwar@aacpinfra.com>
2017-12-25 13:04:47 +0530

    Filled Pages

---
+++ listings.js
@@ -0,0 +1,125 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obmdhbHRpc3dobyIsImEiOiJjajNyZjl3eDYwMWJiMnBvNmlnd3pjcWJqIn0.b-03hU-DxQF21JQC7v5j8g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/johngaltiswho/cjbkz1hlb2pc22spowax5iq7v',
+  center: [77.5946, 12.9716 ],
+  zoom: 9
+});
+
+//Code in order for remove() to work in older browsers
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+map.on('load', function(e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['xre-india-listings']});
+  buildListings(features);
+});
+
+function buildListings(data) {
+  // Iterate through the listings
+  for (i = 0; i < data.length; i++) {
+    var currentFeature = data[i];
+    var prop = currentFeature.properties;
+    // Select the listing container in the HTML and append a div with the class 'item' for each store
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'item';
+    listing.id = 'listing-' + i;
+    // Create a new link with the class 'title' for each store and fill it with the Listing's Name
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.dataPosition = i;
+    link.innerHTML = prop.Name;
+    // Create a new div with the class 'details' for each project and fill it with the Descrioption
+    var details = listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.Type;
+    // Add an event listener for the links in the sidebar listing
+    link.addEventListener('click', function(e) {
+      // Update the currentFeature to the store associated with the clicked link
+      var clickedListing = data[this.dataPosition];
+      // 1. Fly to the point associated with the clicked link
+      flyToProject(clickedListing);
+      // 2. Close all other popups and display popup for clicked store
+      createPopUp(clickedListing);
+      // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+      var activeItem = document.getElementsByClassName('active');
+      if (activeItem[0]) {
+        activeItem[0].classList.remove('active');
+      }
+      this.parentNode.classList.add('active');
+    });
+  }
+}
+
+function flyToProject(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 15
+  });
+}
+
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if (popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>' + currentFeature.properties.Name + '</h3><p>' + currentFeature.properties.Type + '</p>')
+    .addTo(map);
+}
+
+// Add an event listener for when a user clicks on the map
+map.on('click', function(e) {
+  // Query all the rendered points in the view
+  var features = map.queryRenderedFeatures(e.point, { layers: ['xre-india-listings'] });
+  if (features.length) {
+    var clickedPoint = features[0];
+    // 1. Fly to the point
+    flyToProject(clickedPoint);
+    // 2. Close all other popups and display popup for clicked store
+    createPopUp(clickedPoint);
+    // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+    var activeItem = document.getElementsByClassName('active');
+    if (activeItem[0]) {
+      activeItem[0].classList.remove('active');
+    }
+    // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+    var selectedFeature = clickedPoint.properties.Name;
+
+    for (var i = 0; i < features.length; i++) {
+      if (features[i].properties.Name === selectedFeature) {
+        selectedFeatureIndex = i;
+      }
+    }
+    // Select the correct list item using the found index and add the active class
+    var listing = document.getElementById('listing-' + selectedFeatureIndex);
+    listing.classList.add('active');
+  }
+});
+
+var popup = new mapboxgl.Popup({
+    offset: [0, -15],
+    closeButton: false
+});
+
+map.on('mousemove', function(e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['xre-india-listings']});
+  map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+
+  if (!features.length) {
+    popup.remove();
+    return;
+  }
+  var feature = features[0];
+  popup.setLngLat(feature.geometry.coordinates)
+    .setHTML('<h3>' + feature.properties.Name + '</h3><p>' + feature.properties.Type + '</p>')
+    .setLngLat(feature.geometry.coordinates)
+    .addTo(map);
+});
